6a3ddbaf174d9651f27eefa892fc868b57f3af60
Nikhil Gaikwad <162660405+Lthecoders@users.noreply.github.com>
2024-04-29 00:33:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+# print("SECURE LOGIN")
+# username = input("Username > ")
+# password = input("Password> ")
+# if username == "mark" and password=="password":
+#   print("Welcome Mark!")
+# elif username == "suzanne" and password=="light":
+#   print("Hey there Suzanne!")
+# elif username == "jo":
+#   print("Yo Jo!")
+# else:
+#   print("Go away!")
+
+# print("SECURE LOGIN")
+# username = input("Username > ")
+
+# if username == "mark":
+#   print("Welcome Mark!")
+# elif username == "suzanne":
+#   print("Hey there Suzanne!")
+# else:
+#   print("Go away!")
+
+# season = input("what is your favorite season?")
+# if season == "spring":
+#   print("Ah! The birds are chirping and flowers blooming.")
+# elif season == "summer":
+#   print("Catch some sun and cool off with a lemonade.")
+# elif season == "autumn":
+#   print("The leaves are changing and the air is crisp. Enjoy!")
+# elif season == "winter":
+#   print("Stay warm by the fire and watch the snow fall.")
+# else:
+#   print("I don't know that season. Please try again.")
+"---------------👉 Day 6 Challenge---------------"
+
+print("MY LOGIN SYSTEM")
+print("+++++++++++++++")
+print()
+
+username = input("Username > ")
+password = input("Password > ")
+
+if username == "David" and password == "totallyNotB":
+  print("welcome david, today you have to work on day 6 of 100 days of code")
+
+elif username == "suzanne" and password == "totallyNota":
+  print(
+      "welcome suzanne, today you have to watch on day 6 of 100 days of code")
+
+elif username == "jo" and password == "totallyNotj":
+  print(
+      "welcome jo, today you will start working on day 1 of 100 days of code")
+
+else:
+  print("invalid username or password")
